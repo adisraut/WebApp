c2b115b7023cd4ea1b013919fc9812746bfc1249
adityaraut <adityasraut49@gmail.com>
2025-06-12 11:11:18 +0530

    login comp

---
+++ frontend/webapp/src/pages/LoginPage.tsx
@@ -0,0 +1,12 @@
+import LoginForm from '../components/Login/LoginComp';
+
+export function LoginPage() {
+  return (
+    <div style={{ padding: '20px' }}>
+      <h2>Register</h2>
+      <LoginForm />
+    </div>
+  );
+}
+
+export default LoginPage;
